34b6085b9bf8cc0ed74131b08bdb165edad91c37
Angelo <angelojorssatto@gmail.com>
2021-06-13 13:46:49 -0300

    details

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ print('|------------------------------------------------------------------------
 print('|---------- SISTEMA ESPECIALISTA PARA CORRECAO DE pH DE EFLUENTES -----------|')
 print('|----------------------------------------------------------------------------|')
 print('| Desenvolvido por: Angelo Orssatto e Guilherme de Oliveira Felipe           |')
+print('| Especialista consultado: Fabio Orssatto                                    |')
 print('|                                                                            |')
 print('|----------------------------------------------------------------------------|')
 print('| Objetivo do sistema é calcular uma vazao para mistura de uma substancia    |')
